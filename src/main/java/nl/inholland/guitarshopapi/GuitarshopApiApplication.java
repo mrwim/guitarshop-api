36ec1850ffa64438e58841898506623def0f02e8
Wim Wiltenburg <wim.wiltenburg@gmail.com>
2023-04-25 16:19:13 +0200

    Added StockItem as @OneToOne

--- src/main/java/nl/inholland/guitarshopapi/GuitarshopApiApplication.java
+++ src/main/java/nl/inholland/guitarshopapi/GuitarshopApiApplication.java
@@ -1,12 +1,15 @@
 package nl.inholland.guitarshopapi;
 
 import nl.inholland.guitarshopapi.model.Guitar;
+import nl.inholland.guitarshopapi.model.StockItem;
 import nl.inholland.guitarshopapi.repository.GuitarRepository;
+import nl.inholland.guitarshopapi.repository.StockItemRepository;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootApplication
@@ -16,9 +19,8 @@ public class GuitarshopApiApplication {
 		SpringApplication.run(GuitarshopApiApplication.class, args);
 	}
 
-	/*Instead of a database we use this bean ... for now */
 	@Bean
-	public ApplicationRunner runner(GuitarRepository guitarRepository) {
+	public ApplicationRunner runner(GuitarRepository guitarRepository, StockItemRepository stockItemRepository) {
 		return args -> {
 			guitarRepository.saveAll(List.of(
 					new Guitar("Fender", "Stratocaster", 1700.00),
@@ -27,6 +29,15 @@ public class GuitarshopApiApplication {
 			));
 
 			guitarRepository.findAll().forEach(System.out::println);
+
+			List<StockItem> stockItems = new ArrayList<>();
+			guitarRepository.findAll().forEach(
+					g -> stockItems.add(new StockItem(g, 10))
+			);
+
+			stockItemRepository.saveAll(stockItems);
+
+			stockItems.forEach(System.out::println);
 		};
 	}
 }
---
+++ src/main/java/nl/inholland/guitarshopapi/repository/StockItemRepository.java
@@ -0,0 +1,7 @@
+package nl.inholland.guitarshopapi.repository;
+
+import nl.inholland.guitarshopapi.model.StockItem;
+import org.springframework.data.repository.CrudRepository;
+
+public interface StockItemRepository extends CrudRepository<StockItem, Long> {
+}
