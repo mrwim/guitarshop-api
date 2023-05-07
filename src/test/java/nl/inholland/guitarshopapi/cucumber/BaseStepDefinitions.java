2a4004a5407063f40613c241fecec9623f6d8104
Wim Wiltenburg <wim.wiltenburg@gmail.com>
2023-05-07 21:18:14 +0200

    Created cucumber tests

---
+++ src/test/java/nl/inholland/guitarshopapi/cucumber/BaseStepDefinitions.java
@@ -0,0 +1,9 @@
+package nl.inholland.guitarshopapi.cucumber;
+
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@CucumberContextConfiguration
+public class BaseStepDefinitions {
+}
