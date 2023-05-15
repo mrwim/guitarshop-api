4243d240ddffc8b92a7c26ad8a4b5e2c1a3e3529
Ahrnuld <mark@starwave.nl>
2023-05-15 20:29:24 +0200

    Added WithMockUser & dependency

--- src/test/java/nl/inholland/guitarshopapi/controller/BrandControllerTest.java
+++ src/test/java/nl/inholland/guitarshopapi/controller/BrandControllerTest.java
@@ -2,6 +2,7 @@ package nl.inholland.guitarshopapi.controller;
 
 import nl.inholland.guitarshopapi.model.Brand;
 import nl.inholland.guitarshopapi.service.BrandService;
+import nl.inholland.guitarshopapi.util.JwtTokenProvider;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -30,10 +32,14 @@ class BrandControllerTest {
     @MockBean
     private BrandService brandService;
 
+    @MockBean
+    private JwtTokenProvider jwtTokenProvider;
+
     @Value("${application.max.content.size}")
     private int maxSize;
 
     @Test
+    @WithMockUser(username = "admin", roles = {"ADMIN"})
     void callingBrandControllerShouldReturnListOfOneBrand() throws Exception {
         Mockito.when(brandService.getAllBrands())
                 .thenReturn(List.of(
