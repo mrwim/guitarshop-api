cc16e0cd94fc24a420dfd1b65c9cba94b2591084
Wim Wiltenburg <wim.wiltenburg@gmail.com>
2023-05-21 11:47:13 +0200

    Sort by Comparator depending on request parameter

--- src/main/java/nl/inholland/guitarshopapi/controller/GuitarController.java
+++ src/main/java/nl/inholland/guitarshopapi/controller/GuitarController.java
@@ -2,6 +2,7 @@ package nl.inholland.guitarshopapi.controller;
 
 import jakarta.validation.Valid;
 import lombok.extern.java.Log;
+import nl.inholland.guitarshopapi.model.Guitar;
 import nl.inholland.guitarshopapi.model.dto.GuitarDTO;
 import nl.inholland.guitarshopapi.service.GuitarService;
 import org.springframework.http.HttpStatus;
@@ -12,7 +13,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Comparator;
+import java.util.List;
 
 @RestController
 @RequestMapping("guitars")
@@ -27,8 +32,23 @@ public class GuitarController {
 
     @GetMapping
     @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
-    public ResponseEntity<Object> getAllGuitars() {
-        return ResponseEntity.ok(guitarService.getAllGuitars());
+    public ResponseEntity<Object> getAllGuitars(@RequestParam(required = false, defaultValue = "asc") String order) {
+        // The result could just as well - or even better - be accomplished by queries on the database itself
+        List<Guitar> guitars = guitarService.getAllGuitars();
+        Comparator<Guitar> comparator =
+                order.startsWith("desc") ? new Comparator<Guitar>() {
+                    @Override
+                    public int compare(Guitar guitar, Guitar other) {
+                        return (int) (other.getPrice() - guitar.getPrice());
+                    }
+                } : new Comparator<Guitar>() {
+                    @Override
+                    public int compare(Guitar guitar, Guitar other) {
+                        return (int) (guitar.getPrice() - other.getPrice());
+                    }
+                };
+        guitars.sort(comparator);
+        return ResponseEntity.ok(guitars);
     }
 
     @PostMapping
